d76a2dc62de18d41d44dd768650577b76a58fe4c
Ivan Boldyrev <ivan@fluence.one>
2024-03-05 18:43:21 +0100

    feat(shared): implement Display for certain types

--- crates/shared/src/types/result_hash.rs
+++ crates/shared/src/types/result_hash.rs
@@ -67,3 +67,9 @@ impl ToHex for ResultHash {
         ToHex::encode_hex_upper(&self.0)
     }
 }
+
+impl std::fmt::Display for ResultHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.encode_hex::<String>())
+    }
+}
